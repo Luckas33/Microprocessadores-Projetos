a4b0f707025551ba722d7c9abddd41f254d6876f
Carlos Vinícius <162610513+CaviSantos@users.noreply.github.com>
2025-01-16 22:15:09 -0300

    OPCODES ATUALIZADOS

--- Projeto_1/Arquitetura_Multiprocessada/Compiler.cpp
+++ Projeto_1/Arquitetura_Multiprocessada/Compiler.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <algorithm>
-#include <fstream>
 #include <map>
 
-#define Word_size 8
+#define WORD_SIZE 8
 
 using namespace std;
 
-struct info{
+struct info {
     string opcode;
     int arguments;
 };
 
-map<string,info>codigos = {
-    {"ADD", info{"00000001"},2},
-    {"SUB", info{"00000010"},2},
-    {"NOT", info{"00000100"},2}
-    //ADICIONAR MAIS CODIGOS DE OPERAÇÃO AQUI!
+map<string, info> mnemonics = {
+
+//MODIFIQUE OS MINEMONICOS SE FOR NECESSÁRIO,SE  QUISER  IMPLEMENTAR MAIS ETC...
+    { "ADD", info{"00000001", 1} },  // ADD
+    { "SUB", info{"00000010", 1} }, // SUB
+    { "MUL", info{"00000011", 1} }, // MUL
+    { "DIV", info{"00000100", 1} }, // DIV
+    { "MOD", info{"00000101", 2} }, // MOD  
+    { "AND", info{"00000110", 2} },  // AND
+    { "OR", info{"00000111", 2} },  //OR
+    { "XOR", info{"00001000", 2} }, //XOR
+    { "NOT", info{"00001001", 2} }, //NOT
+    { "NOR", info{"00001010", 2} }, //NOR
+    { "NAND" , info{"00001011", 2} }, //NAND
+    { "XNOR", info{"10001100", 2} },  //XNOR
 };
-void removeCommentsAndSpaces(string &s) {
-    // Remove comentários (tudo após o ponto e vírgula)
+
+void removeComments(string &s) {
     size_t pos = s.find(";");
     if (pos != string::npos) s.erase(pos);
+}
 
-    // Remove todos os espaços
+void removeSpaces(string &s) {
     s.erase(remove(s.begin(), s.end(), ' '), s.end());
 }
 
-//verifica se o argumento é binario
 bool isBinary(const string& s) {
     return !s.empty() && s.find_first_not_of("01") == string::npos;
 }
@@ -45,84 +55,87 @@ string getOpcode(const string& s) {
     return mnemonics[s].opcode;
 }
 
-int getArguments(const string& s) {
-    return mnemonics[s].arguments;
+string completeArgument(const string& binary) {
+    size_t missingZeros = WORD_SIZE - binary.size();
+    string fullBinary = string(missingZeros, '0') + binary;
+    return fullBinary;
 }
 
-string getOpcode(const string& s) {
-    return mnemonics[s].opcode;
-}
+int main(int argc, char** argv) {
 
-
-int main(int agrc, char**argv){
-
-    if(argc != 3){
-        cerr << "Usage: ./compilador file.asm file.bin"<<endl;
+    if (argc != 3) {
+        cerr << "Usage: ./compiler file.asm file.bin" << endl;
         return 1;
     }
-    ifstream asm_file(argv[1]);
-    if(!asm_file.is_open()){
-        cerr << "cannot open"<<argv[1]<<endl;
+
+    // Usando o caminho absoluto para os arquivos
+    ifstream asm_file("C:/Users/maria/OneDrive/Desktop/compilador/input.asm"); // Caminho absoluto do arquivo de entrada
+    if (!asm_file.is_open())
+    {
+        cerr << "Cannot open input.asm" << endl;
         return 2;
     }
-    ofstream bin_file(argv[2],ios::out | ios::binary);
-    if(!bin_file.is_open()){
-        cerr << "cannot open"<<endl;
+
+    ofstream bin_file("C:/Users/maria/OneDrive/Desktop/compilador/output.bin", ios::out | ios::binary); // Caminho absoluto do arquivo de saída
+    if (!bin_file.is_open()) {
+        cerr << "Cannot open output.bin" << endl;
         asm_file.close();
         return 2;
     }
 
-    string cmdBuffer, argbuffer;
+    string cmdBuffer, argBuffer;
     int line = 0;
 
-    //esse loop faz a procura dos codigos.
-    while(getline(asm_file,cmdBuffer)){
+    // Loop principal: procura comandos que são representados por mnemonicos válidos
+    while (getline(asm_file, cmdBuffer)) {
+
         line++;
-        removeCommentsAndSpaces(cmdBuffer);
+        removeComments(cmdBuffer); removeSpaces(cmdBuffer);
 
-        if(cmdBuffer.empty())continue;
+        if (cmdBuffer.empty()) continue;
 
-        if(!isCommand(cmdBuffer)){
-            cerr <<"semantic error in line "<< line << ": command "<<cmdBuffer<< "is undefined" endl;
-            asm_file.close(); bin_file.close;
+        if (!isCommand(cmdBuffer)) {
+            cerr << "Semantic Error in line " << line << ": Command " << cmdBuffer << " is undefined" << endl;
+            asm_file.close(); bin_file.close();
             return 3;
         }
 
         int expectedArgs = getArguments(cmdBuffer);
         string opcode = getOpcode(cmdBuffer);
 
-        bin_file.write(opcode.c_str(),opcode.size());
+        bin_file.write(opcode.c_str(), opcode.size());
         bin_file.put('\n');
 
-        //esse loop aqui analisa se as linhas que seguem são do padrão correto para fazer as operações
-        for(int i=0;i<expectedArgs;i++){
-            line++
+        // Loop interno: analisa argumentos (linhas seguintes) para os comandos
+        for (int i = 0; i < expectedArgs; i++) {
 
-            if(!getline(asm_file,argbuffer)){
+            line++;
+
+            if (!getline(asm_file, argBuffer)) {
                 cerr << "EOF reached before expected arguments" << endl;
-                asm_file.close(); bin_file.close;
+                asm_file.close(); bin_file.close();
                 return 4;
             }
 
-            removeCommentsAndSpaces(argbuffer);
-            if(argbuffer.empty()){
+            removeComments(argBuffer); removeSpaces(argBuffer);
+
+            if (argBuffer.empty()) {
                 i--; continue;
             }
-            //verifica se o argbuffer é binario e se o tamanho do argumento é do tamanho aceitado
-            if(!isBinary(argbuffer)||argbuffer.size() > Word_size){
+
+            if (!isBinary(argBuffer) || argBuffer.size() > WORD_SIZE) {
                 cerr << "Syntactic Error in line " << line << ": Invalid argument " << argBuffer << " for command " << cmdBuffer << endl;
                 cerr << "Expected binary argument of size " << WORD_SIZE << endl;
                 asm_file.close(); bin_file.close();
                 return 5;
             }
 
-            argbuffer = completeArgument(argbuffer);
-            bin_file.write(argbuffer.c_str(),argbuffer.size());
+            argBuffer = completeArgument(argBuffer);
+            bin_file.write(argBuffer.c_str(), argBuffer.size());
             bin_file.put('\n');
         }
     }
-    //fecha tudo
-    asm_file.close();
-    bin_file.close();
+
+    asm_file.close(); bin_file.close();
     return 0;
 }
