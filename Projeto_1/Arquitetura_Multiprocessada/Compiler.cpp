1086e5550065ccb15c2a37e9bb528bf315d445c7
MateusAndrMaia <150792464+MateusAndrMaia@users.noreply.github.com>
2025-01-19 23:40:39 -0300

    Update Compiler.cpp

--- Projeto_1/Arquitetura_Multiprocessada/Compiler.cpp
+++ Projeto_1/Arquitetura_Multiprocessada/Compiler.cpp
@@ -28,6 +28,9 @@ map<string, info> mnemonics = {
     { "NOR", info{"00001010", 2} }, //NOR
     { "NAND" , info{"00001011", 2} }, //NAND
     { "XNOR", info{"10001100", 2} },  //XNOR
+    { "NOR", info{"00001101", 2} }, //MOV
+    { "NAND" , info{"00001110", 2} }, //SLL
+    { "XNOR", info{"10001111", 2} },  //SRL
 };
 
 void removeComments(string &s) {
